5df21e8027d1e0e04186b4a06868da651cccf86e
BarrettGuaN <barrettguan@gmail.com>
2018-03-31 16:15:44 +0800

    第一次版本提交

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("请输入要登记的学生个数：");
+            int i=0;
+            try
+            {
+                i = Convert.ToInt32(Console.ReadLine());
+                if(i==0)
+
+                {
+                    throw new Exception("输入为0.");
+                }
+            }
+            catch (Exception Mes)
+            {
+                Console.WriteLine(Mes.Message+"必须输入不为0的数字。");
+            }
+            Console.WriteLine("请输入要登记的学生姓名：");
+            string[] name = new string[i];
+            try
+            {
+                int n = 0;
+                for (int j = 0; j < i; j++)
+                {
+                    
+                    name[j] = Console.ReadLine();
+                    if (name[j] == "刘博" || name[j] == "王雨晴" || name[j] == "刘宇行")
+                    {
+                        n = n + 1;                       
+                        Console.WriteLine(name[j] + "是小猪！");
+                        if (n == 3)
+                        {
+                            Console.WriteLine("三只小猪齐了！");
+                        }
+                    }
+                    if (name[j]=="关李"|| name[j] == "孙权"|| name[j] == "王明礼")
+                    {
+                        Console.WriteLine(name[j]+"是帅哥！");
+                    }
+                }
+            }
+            catch (Exception Mes)
+            {
+
+                Console.WriteLine(Mes.Message);
+            }
+            Console.WriteLine("已登记的学生如下：");
+            foreach (string s in name)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    public class teacher
+    {
+        public string name;
+        public string title;
+        static public string department;
+        public void show()
+        {
+            Console.WriteLine("姓名：{0}\n职称：{1}",name,title);
+        } 
+        public static void showD()
+        {
+            Console.WriteLine("所属院系：{0}",department);
+        }
+    }
+
+    class Program
+    {
+        static teacher getTeacher(int i)
+        {
+            teacher a = new teacher();
+            Console.Write("请输入第{0}位的姓名：", i + 1);
+            a.name = Console.ReadLine();
+            Console.Write("请输入第{0}位的职称：", i + 1);
+            a.title = Console.ReadLine();
+            return a;
+        }
+        static void putTeacher(teacher a,int i)
+        {
+            Console.WriteLine("第{0}位的信息为：", i + 1);
+            a.show();
+            teacher.showD();
+        }
+
+
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Console.Write("请输入要登记的人数：");
+            try
+            {
+                count = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.Message);
+            }
+            Console.WriteLine();
+
+            teacher[] a = new teacher[count];
+
+            Console.Write("请输入所属院系：");
+            teacher.department = Console.ReadLine();
+
+            for(int i=0;i<count;i++)
+            {
+                a[i] = getTeacher(i);
+            }
+
+            for(int i=0;i<count;i++)
+            {
+                putTeacher(a[i],i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
